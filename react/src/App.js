b3af3a6d693ba9773bc51ebcb41c238efa423c02
Leela Anuroop A <38076999+leelaakula@users.noreply.github.com>
2021-03-14 23:50:22 +0530

    sass files basic implementation

--- react/src/App.js
+++ react/src/App.js
@@ -1,30 +1,28 @@
 import './App.scss'
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <nav>
-        <ul className='navigation'>
-          <li>
-            <a href='#'>About us</a>
-          </li>
-          <li>
-            <a href='#'>Pricing</a>
-          </li>
-          <li>
-            <a href='#'>Contact</a>
-          </li>
-        </ul>
-        <div class='button'>
-          <a class='btn btn-main' href='#'>
-            Sign Up
-          </a>
-          <a class='btn btn-hot' href='#'>
-            Get a Quote
-          </a>
-        </div>
-      </nav>
-    </div>
+    <nav>
+      <ul className='navigation'>
+        <li>
+          <a href='#'>About us</a>
+        </li>
+        <li>
+          <a href='#'>Pricing</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+      </ul>
+      <div class='buttons'>
+        <a class='btn btn-main' href='#'>
+          Sign Up
+        </a>
+        <a class='btn btn-hot' href='#'>
+          Get a Quote
+        </a>
+      </div>
+    </nav>
   )
 }
 
